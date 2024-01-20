87f1f8123bb29a581ca38f70b3683b2bbd1cf5e9
Andy Aoe <andy@level.co>
2024-01-20 13:51:11 -0800

    loading gray scale image

---
+++ Illuminant/Program.cs
@@ -0,0 +1,8 @@
+﻿using Emgu.CV.Structure;
+using Emgu.CV;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Image<Gray, Byte> img1 = new Image<Gray, Byte>("gray.png");
+Console.Write("Width={0} Height={1}", img1.Size.Width, img1.Size.Height);
+
